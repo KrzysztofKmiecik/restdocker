1d1c549b759f998af6b377d2239b51e44697e793
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2020-10-13 02:04:57 +0200

    myproperties

--- src/main/java/com/example/restdocker/Controller.java
+++ src/main/java/com/example/restdocker/Controller.java
@@ -1,15 +1,17 @@
 package com.example.restdocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Value("${envname}")
+    private String envname;
 
     @GetMapping("/hello")
-
-    public String writeHello(){
-        return "Hello!";
+    public String writeHello() {
+        return "Hello!" + envname;
     }
 
 }
